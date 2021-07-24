e8bfb1f16a3a105c37ef225d2b019d789916308c
alcohol-korea <han071416@naver.com>
2021-07-24 17:04:13 +0900

    30days challenge
    JS Drum Kit

--- 30days-cahllenge/day1.js
+++ 30days-cahllenge/day1.js
@@ -1,8 +1,19 @@
-const key =document.querySelector(".key");
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //화면에 없는키는 null로 나오므로 아얘 안되게 한다.
+    audio.currentTime = 0;//audio가 시작하는 기간을 0으로 만들어서  되감기함
+    audio.play(); //audio실행
+    key.classList.add("playing");
+}
+function removeTransition(e){
+    if(e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach(function(key){
+    key.addEventListener("transitionend",removeTransition);
+});
 
-function soundPlay(e){
-    console.dir(e.path[0].attributes[0].value);
-};
 
-window.addEventListener("click",soundPlay)
-
+window.addEventListener("keydown",playSound);
